dba49714090c380bf2ac811717f7a4aa14b4d2d8
awesh.gautam31@gmail.com <awesh@2000>
2020-06-13 20:06:46 +0530

    Updates

---
+++ precache-manifest.43f1706b0127d56d510047fe71b99434.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12efade3116bb583707d62b241fa41d9",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "1ddf2664e03ecd6d195b",
+    "url": "/Portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dae188a2b10ccf134d2a",
+    "url": "/Portfolio/static/css/main.e84c5199.chunk.css"
+  },
+  {
+    "revision": "1ddf2664e03ecd6d195b",
+    "url": "/Portfolio/static/js/2.08a7e00e.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/Portfolio/static/js/2.08a7e00e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae188a2b10ccf134d2a",
+    "url": "/Portfolio/static/js/main.0d322826.chunk.js"
+  },
+  {
+    "revision": "425e952245b014fc3ffb",
+    "url": "/Portfolio/static/js/runtime-main.db605a1d.js"
+  },
+  {
+    "revision": "35f8a7315703dd6c80c0de95aef15bae",
+    "url": "/Portfolio/static/media/avatar.35f8a731.jpg"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/Portfolio/static/media/background.2dd213aa.jpg"
+  },
+  {
+    "revision": "234a691f13a9f71a846e995303eef2e7",
+    "url": "/Portfolio/static/media/cityframe.234a691f.jpg"
+  },
+  {
+    "revision": "ccb736a2c25a4f887491a4dd81223e4b",
+    "url": "/Portfolio/static/media/greeting.ccb736a2.png"
+  },
+  {
+    "revision": "a4abf1785a53d874ec1d93ae5471fc57",
+    "url": "/Portfolio/static/media/project1.a4abf178.jpg"
+  },
+  {
+    "revision": "f5be44cf00fd49d82400c5b95745358c",
+    "url": "/Portfolio/static/media/scaryframe.f5be44cf.jpg"
+  },
+  {
+    "revision": "ca0d4180ab6c4d0d855b2ab821df51e6",
+    "url": "/Portfolio/static/media/shivratri1.ca0d4180.jpg"
+  }
+]);
